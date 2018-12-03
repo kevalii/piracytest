f2c3ca2779bedadecc209e886c172b89e5ea5641
Alex R <alexrankine@college.harvard.edu>
2018-12-03 02:52:39 -0500

    prepare for production push

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from tools.doc import get_docx, get_text
 from os import path, urandom
 from werkzeug.utils import secure_filename
 from datetime import datetime
+from docx import Document
 
 UPLOAD_FOLDER = 'usrfiles'
 ALLOWED_EXTENSIONS = set(['.txt', '.docx'])
@@ -16,12 +17,30 @@ app = Flask(__name__)
 app.secret_key = urandom(16)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ## For testing
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/translations'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/translations'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 # Production env database setup
-# heroku = Heroku(app)
+heroku = Heroku(app)
 db = SQLAlchemy(app)
+
+# Easiest to include these functions in the main app.py
+# Extract and translate text from the paragraphs of a .docx file
+def get_docx(filename):
+	doc = Document(f'{UPLOAD_FOLDER}/{filename}')
+	for para in doc.paragraphs:
+		para.text = get_translated(para.text)
+	doc.save(f'{UPLOAD_FOLDER}/{filename}')
+
+
+# Extract and translate text of a .txt file
+def get_text(filename):
+	with open(f'{UPLOAD_FOLDER}/{filename}', 'r+') as file:
+		text = file.read()
+		file.seek(0)
+		file.write(get_translated(text))
+		file.truncate()
+
 
 
 # Message model
